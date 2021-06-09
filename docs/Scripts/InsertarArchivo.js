69bcadd12d2bb64ccd8defb95366f40b1fdacd9b
Astrid Martinez <astrid_martinez60@yahoo.es>
2021-06-09 15:59:07 -0600

    interfaz grafica

--- docs/Scripts/InsertarArchivo.js
+++ docs/Scripts/InsertarArchivo.js
@@ -1,3 +1,12 @@
+var nombreEntrada ='';
+var textA = document.getElementById("editor");
+var editor = CodeMirror.fromTextArea(textA, {
+  lineNumbers: true,
+mode: "text/html",
+matchBrackets: true
+});
+
+
 var contenidoGeneral ;
 
 var inputElement = document.getElementById("abrir");
@@ -12,6 +21,8 @@ function ingresarTexto(item) {
 function leerArchivo(e) {
     
     var archivo = e.target.files[0];
+    nombreEntrada = e.target.files[0].name;
+    console.log( nombreEntrada)
     if (!archivo) {
       return;
     }
@@ -26,13 +37,55 @@ function leerArchivo(e) {
 };
 
 function mostrarContenido(contenido){
-    var editor = document.getElementById('editor');
-    editor.value=contenido;
+    editor.setValue(contenido);
 }
 
-function limpiar(){
-    var editor = document.getElementById('editor');
-    editor.value="";
+
+function limpiar() {
+  editor.setValue('');
+  nombreEntrada = null;
+  document.getElementById("abrir").value = '';
+
+}
+
+
+var num = 0;
+function guardarEntrada() {
+  console.log(editor.getValue());
+  var text = editor.getValue();
+  if (nombreEntrada != null) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', nombreEntrada);
+
+    if (document.createEvent) {
+      var event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      pom.dispatchEvent(event);
+    }
+    else {
+      pom.click();
+    }
+  } 
+}
+
+function guardarResultado() {
+  var nombreRes= 'Resultado Consulta = '+num+".txt";
+  var text = document.getElementById('resultado').value;
+  if (nombreRes != null) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', nombreRes);
+
+    if (document.createEvent) {
+      var event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      pom.dispatchEvent(event);
+    }
+    else {
+      pom.click();
+    }
+  } 
 }
 
 
